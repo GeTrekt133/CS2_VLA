26214d63b47ceebb3d5692b2cb42cdd7088caa8c
GeTrekt133 <154702977+GeTrekt133@users.noreply.github.com>
2026-01-04 00:40:41 +0300

    Refactor Dataloader to use DatasetIntent and add debug
    Updated dataset import and added debug collate function.

--- src/Dataloader.py
+++ src/Dataloader.py
@@ -2,10 +2,18 @@ import torch
 from torch.utils.data import Sampler
 from torch.utils.data import DataLoader
 from collections import defaultdict
-from Dataset import CSRoundDataset
+# from Dataset import CSRoundDataset
+from DatasetIntent import CSRoundDataset
 from tqdm import tqdm
 import random
 
+
+def debug_collate(batch):
+    for k in batch[0].keys():
+        shapes = [b[k].shape for b in batch if torch.is_tensor(b[k])]
+        if len(set(shapes)) > 1:
+            print(f"[COLLATE ERROR] {k}: {shapes}")
+    return torch.utils.data.default_collate(batch)
 
 class ExactMatchBucketSampler(Sampler):
     def __init__(self, dataset, batch_size, shuffle=True):
@@ -23,7 +31,7 @@ class ExactMatchBucketSampler(Sampler):
 
         # фильтруем маленькие бакеты
         self.bucket_keys = [k for k, v in self.buckets.items() if len(v) >= batch_size]
-        print(f"[INFO] Using {len(self.bucket_keys)} buckets")
+        print(f"[INFO] Using {len(self.bucket_keys)} buckets: {self.bucket_keys}")
 
     def __iter__(self):
         all_batches = []
@@ -32,6 +40,11 @@ class ExactMatchBucketSampler(Sampler):
             if self.shuffle:
                 random.shuffle(indices)
             batches = [indices[i:i+self.batch_size] for i in range(0, len(indices), self.batch_size)]
+            if len(batches[-1]) != self.batch_size:
+                batches = batches[:-1]
+            # if len(batches) > 0:
+            #     print(key)
+            #     break
             all_batches.extend(batches)
         if self.shuffle:
             random.shuffle(all_batches)
@@ -42,15 +55,23 @@ class ExactMatchBucketSampler(Sampler):
 
 
 if __name__ == "__main__":
-    dataset = CSRoundDataset(r"C:\Users\misas\CS2_NN\train_dataset.json", sampler=True)
+    # dataset = CSRoundDataset(r"C:\Users\misas\CS2_NN\train_dataset.json", sampler=True)
+    dataset = CSRoundDataset(r"/mnt/ml/msirotkin/shock2/train_dataset.json", sampler=True)
     sampler = ExactMatchBucketSampler(dataset, batch_size=4, shuffle=True)
-    dataset = CSRoundDataset(r"C:\Users\misas\CS2_NN\train_dataset.json")
+    # dataset = CSRoundDataset(r"C:\Users\misas\CS2_NN\train_dataset.json")
+    dataset = CSRoundDataset(r"/mnt/ml/msirotkin/shock2/train_dataset.json")
     loader = DataLoader(dataset, 
                         batch_sampler=sampler,
+                        collate_fn=debug_collate,
                         num_workers=4)
 
     for i, sample in tqdm(enumerate(loader)):
-        print(i, sample["game_id"], sample["round_id"])
-        print(sample["scene_seq"].shape, sample["radar_seq"].shape, sample["state_vec"].shape, sample["actions_mouse"].shape, sample["actions_keys"].shape, sample["labels_mouse"].shape, sample["labels_keys"].shape)
+        # print(i, sample["game_id"], sample["round_id"])
+        # print(sample["scene_seq"].shape, sample["radar_seq"].shape, sample["state_vec"].shape, sample["actions_mouse"].shape, sample["actions_keys"].shape, sample["labels_mouse"].shape, sample["labels_keys"].shape)
+        print(sample["scene_seq"].shape, sample["radar_seq"].shape, sample["intent"].shape, sample["actions_mouse"].shape, sample["actions_keys"].shape, sample["target_mouse"].shape, sample["T"].shape)
+        # print(sample["intent"])
+        # print(sample["target_mouse"])
+        # print(sample["actions_keys"], sample["actions_keys"].shape)
+        # print(sample["actions_mouse"], sample["actions_mouse"].shape)
         if i == 200:
             break
